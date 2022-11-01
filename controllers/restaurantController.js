4c02ae7422ff56a5d920434aed680549f76d6b30
zu3x <zu3x@fresnou.org>
2022-11-01 11:19:11 -0700

    Third Commit

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,9 +1,28 @@
 const {Restaurant} = require('../models')
-const categories = ['Japanese', 'Pizza', 'Fast Food', 'Italian', 'Chinese', 'Hawaiian', 'American'];
+const categories = ['Japanese', 'Pizza', 'Fast Food', 'Italian', 'Chinese', 'Hawaiian', 'American', 'Sandwiches', 'Vietnamese', 'Asian', 'Mexican'];
 
 module.exports.viewAll = async function(req, res, next) {
-    const restaurants = await Restaurant.findAll();
-    res.render('index', {restaurants});
+    let searchCategories = ['All'];
+    for (let i = 0; i<categories.length; i++){
+        searchCategories.push(categories[i]);
+    }
+    let restaurants;
+    let searchCategory = req.query.category || 'All';
+    let searchRandom = req.query.random || false;
+    if (searchCategory==='All'){
+        restaurants = await Restaurant.findAll();
+    } else {
+        restaurants = await Restaurant.findAll({
+            where: {
+                category: searchCategory
+            }
+        });
+    }
+    if (restaurants.length > 0 && searchRandom) {
+        let randomIndex = getRandomInt(restaurants.length);
+        restaurants = [restaurants[randomIndex]];
+    }
+    res.render('index', {restaurants, categories:searchCategories, searchCategory, searchRandom});
 }
 
 module.exports.renderEditForm = async function(req, res, next) {
@@ -63,4 +82,7 @@ module.exports.addRestaurant = async function(req, res) {
             description: req.body.description
         });
     res.redirect('/');
+}
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
 }
